cb5c2f3e987d106c781215e8a63ad0d127af830a
Neetesh kurmi <kurmineetesh15@gmail.com>
2026-08-15 03:19:32 +0530

    Time: 50 ms (56.73%), Space: 55.2 MB (98.42%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,18 @@
+var createCounter = function(init) {
+    let current = init;
+
+    return {
+        increment: function() {
+            return ++current;
+        },
+
+        decrement: function() {
+            return --current;
+        },
+
+        reset: function() {
+            current = init;
+            return current;
+        }
+    };
+};
