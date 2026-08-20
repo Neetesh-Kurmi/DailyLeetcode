563113b0e8a08bbea268192ea8f52caf5e4df80c
Neetesh kurmi <kurmineetesh15@gmail.com>
2026-08-21 03:15:52 +0530

    Time: 987 ms (73.35%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -2,9 +2,8 @@
 SELECT
 r.contest_id,
 ROUND(count(r.user_id)*100.0/(SELECT count(*) from Users),2 ) AS percentage
-FROM Users u
-JOIN Register r
-ON u.user_id = r.user_id
+FROM Register r
+
 Group by r.contest_id
 ORDER BY percentage DESC , r.contest_id ;
 
