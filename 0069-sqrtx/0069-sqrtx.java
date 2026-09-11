a4491ada23a0e0a592b899a3c820c382a1949723
Neetesh kurmi <kurmineetesh15@gmail.com>
2026-09-12 04:47:45 +0530

    Time: 1 ms (99.08%), Space: 42.5 MB (61.35%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        int start = 1;
+        int end = x;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            long sqrt = (long)mid*mid;
+            if(sqrt==x){
+                return mid;
+            }
+            else if(sqrt<x){
+                start= mid+1;
+            }
+            else end = mid-1;
+
+        }
+        return end;
+        
+    }
+}
